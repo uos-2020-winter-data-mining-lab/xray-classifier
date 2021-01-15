0f114b35cfcfbd6ad19d32759cfd50bc73df22e5
handal95 <45425838+handal95@users.noreply.github.com>
2021-01-15 15:50:46 +0900

    generator refactor

--- app/hans/models/train.py
+++ app/hans/models/train.py
@@ -7,10 +7,9 @@ import keras.backend as K
 from keras.layers import Input, Lambda
 from keras.models import Model
 from keras.optimizers import Adam
-from keras.callbacks import ReduceLROnPlateau, EarlyStopping
 
-from .model import preprocess_true_boxes, yolo_body, yolo_loss
-from .utils import get_random_data
+from .model import yolo_body, yolo_loss
+from app.hans.config import INPUT_SHAPE, NUM_CLASSES
 
 
 def get_classes(classes_path):
@@ -29,67 +28,50 @@ def get_anchors(anchors_path):
     return np.array(anchors).reshape(-1, 2)
 
 
-def create_model(input_shape, anchors, num_classes, freeze_body=2):
+def create_model(
+    anchors, load_pretrained=True, freeze_body=2, weights='data/yolov3.h5'
+):
     '''create the training model'''
     K.clear_session()  # get a new session
     image_input = Input(shape=(None, None, 3))
-    h, w = input_shape
+    h, w = INPUT_SHAPE
     num_anchors = len(anchors)
 
     y_true = [
         Input(shape=(
             h//{0: 32, 1: 16, 2: 8}[layer],
             w//{0: 32, 1: 16, 2: 8}[layer],
-            num_anchors//3,
-            num_classes+5)
+            num_anchors // 3,
+            NUM_CLASSES + 5)
         ) for layer in range(3)
     ]
 
-    model_body = yolo_body(image_input, num_anchors//3, num_classes)
-    print('Create YOLOv3 model with {} anchors and {} classes.'.format(
-        num_anchors, num_classes))
+    model_body = yolo_body(image_input, num_anchors//3, NUM_CLASSES)
+    print(f'Create YOLOv3 model with {num_anchors} anchors '
+          f'and {NUM_CLASSES} classes.')
 
-    model_loss = Lambda(yolo_loss, output_shape=(1,), name='yolo_loss',
-                        arguments={'anchors': anchors, 'num_classes': num_classes, 'ignore_thresh': 0.5})(
-        [*model_body.output, *y_true])
+    if load_pretrained is True:
+        model_body.load_weights(weights, by_name=True, skip_mismatch=True)
+        print(f'Load weights "{weights}"')
+        if freeze_body in [1, 2]:
+            # Freeze darknet53 body or freeze all but 3 output layers.
+            num = (185, len(model_body.layers)-3)[freeze_body-1]
+            for i in range(num):
+                model_body.layers[i].trainable = False
+            print(f'Freeze the First {num} layers of '
+                  f'total {len(model_body.layers)}')
+
+    model_loss = Lambda(
+        yolo_loss,
+        output_shape=(1,),
+        name='yolo_loss',
+        arguments={
+            'anchors': anchors,
+            'num_classes': NUM_CLASSES,
+            'ignore_thresh': 0.5
+        }
+    )([*model_body.output, *y_true])
+
     model = Model([model_body.input, *y_true], model_loss)
 
     return model
-
-
-def data_generator(
-    annotation_lines, batch_size, input_shape, anchors, num_classes
-):
-    '''data generator for fit_generator'''
-    n = len(annotation_lines)
-    i = 0
-    while True:
-        image_data = []
-        box_data = []
-        for b in range(batch_size):
-            if i == 0:
-                np.random.shuffle(annotation_lines)
-            image, box = get_random_data(
-                annotation_lines[i], input_shape, random=True)
-            image_data.append(image)
-            box_data.append(box)
-            i = (i+1) % n
-        image_data = np.array(image_data)
-        box_data = np.array(box_data)
-        y_true = preprocess_true_boxes(
-            box_data, input_shape, anchors, num_classes)
-        yield [image_data, *y_true], np.zeros(batch_size)
-
-
-def data_generator_wrapper(
-    annotation_lines, batch_size, input_shape, anchors, num_classes
-):
-    n = len(annotation_lines)
-    if n == 0 or batch_size <= 0:
-        return None
-    return data_generator(
-        annotation_lines, batch_size, input_shape, anchors, num_classes)
-
-
-if __name__ == '__main__':
-    _main()
--- app/hans/run.py
+++ app/hans/run.py
@@ -4,11 +4,13 @@ import tensorflow as tf
 from keras.optimizers import Adam
 from keras.callbacks import ReduceLROnPlateau, EarlyStopping
 
-from app.hans.config import INPUT_SHAPE
+from app.hans.config import INPUT_SHAPE, BATCH_SIZE
 from app.hans.plot import plotting
 from .models.train import create_model
-from .models.model import preprocess_true_boxes
-from .models.utils import get_random_data
+from .utils.data import read_file
+from .utils.split import data_split, data_generator
+
+
 DATA_DIR = 'data/'
 RAW_DATA = f'{DATA_DIR}/raw/dataset'
 
@@ -22,145 +24,49 @@ tf.compat.v1.keras.backend.set_session(session)
 
 
 def run():
-    # Step 0 . Path
-    annotation_path = os.path.join(DATA_DIR, 'train.txt')
-    with open(annotation_path) as f:
-        lines = f.readlines()
+    # Step 0 . Path Config
+    data = read_file('annotation')
+    class_names, num_classes = read_file('classes')
+    anchors = read_file('anchors')
 
-    classes_path = os.path.join(DATA_DIR, 'labels.txt')
-    class_names = get_classes(classes_path)
-    num_classes = len(class_names)
+    # Step 1 . Data Preprocessing
+    (train_data, valid_data), (num_train, num_valid) = data_split(data)
 
-    anchors_path = os.path.join(DATA_DIR, 'anchors.txt')
-    anchors = get_anchors(anchors_path)
+    # Step 2. Data Generating
+    train_generator = data_generator(train_data, anchors)
+    valid_generator = data_generator(valid_data, anchors)
 
-    # Step 1 . Model Config
-    input_shape = INPUT_SHAPE
-
-    model = create_model(input_shape, anchors, num_classes, freeze_body=2)
+    # Step 3. Model Setting
+    model = create_model(anchors, freeze_body=2, weights='data/yolov3-320.h5')
     # model.summary(positions=[.35, .65, .73, 1.])
 
-    val_split = 0.1
-    np.random.seed(10101)
-    np.random.shuffle(lines)
-    np.random.seed(None)
+    # Step 4. Model Compile
+    # model.compile(
+    #     optimizer=Adam(lr=1e-4),
+    #     loss={'yolo_loss': lambda y_true, y_pred: y_pred},
+    # )
 
-    num_valid = int(len(lines) * val_split)
-    num_train = len(lines) - num_valid
+    # Step 5. Model Fitting
+    # reduce_lr = ReduceLROnPlateau(factor=0.1, patience=3, verbose=1)
+    # early_stopping = EarlyStopping(min_delta=0, patience=10, verbose=1)
 
-    model.compile(
-        optimizer=Adam(lr=1e-4),
-        loss={'yolo_loss': lambda y_true, y_pred: y_pred}
-    )
+    # history = model.fit(
+    #     train_generator,
+    #     steps_per_epoch=max(1, num_train//batch_size),
+    #     validation_data=valid_generator,
+    #     validation_steps=max(1, num_valid//batch_size),
+    #     epochs=100,
+    #     callbacks=[reduce_lr, early_stopping])
 
-    batch_size = 8
-    print(f'Train on {num_train} samples, '
-          f'val on {num_valid} samples, '
-          f'with batch size {batch_size}.')
+    # Step 6. Plotting
+    # plotting(history)
 
-    train_generator = data_generator_wrapper(
-        lines[:num_train], batch_size, input_shape, anchors, num_classes)
+    # Step 7. Predict / Evaluate
+    # results = model.evaluate(test_data, test_labels)
 
-    valid_generator = data_generator_wrapper(
-        lines[num_train:], batch_size, input_shape, anchors, num_classes)
-
-    reduce_lr = ReduceLROnPlateau(factor=0.1, patience=3, verbose=1)
-    early_stopping = EarlyStopping(min_delta=0, patience=10, verbose=1)
-
-    for i in range(len(model.layers)):
-        model.layers[i].trainable = True
-
-    history = model.fit(
-        train_generator,
-        steps_per_epoch=max(1, num_train//batch_size),
-        validation_data=valid_generator,
-        validation_steps=max(1, num_valid//batch_size),
-        epochs=100,
-        callbacks=[reduce_lr, early_stopping])
-
-    # Step0. config
-    # RAW_DIR = os.path.join(DATA_DIR, 'raw\\dataset\\')
-    # TRAIN_DIR = os.path.join(DATA_DIR, 'train\\')
-    # VALID_DIR = os.path.join(DATA_DIR, 'valid\\')
-    # TEST_DIR = os.path.join(DATA_DIR, 'test\\')
-
-    # # Step1. Data Preparing
-    # init_flag = False
-    # print("DATA COPYING...")
-    # if init_flag is True:
-    #     copy_data(RAW_DIR)
-
-    #     preprocess_data(TRAIN_DIR)
-    #     preprocess_data(VALID_DIR)
-
-    # # Step2. Data Generating
-    # print("DATA GENERATING...")
-    # train_generator = generator(TRAIN_DIR)
-    # valid_generator = generator(VALID_DIR)
-
-    # # Step5. Predict / Evaluate
-    # # results = model.evaluate(test_data, test_labels)
-
-    # # Step6. Plotting
-    plotting(history)
-
-    # # Step7. Output
+    # Step 8. Output
     # print("RUN XRAY DATA \n epochs : {EPOCHS} \n")
 
 
 if __name__ == '__main__':
     run()
-
-
-def get_classes(classes_path):
-    '''loads the classes'''
-    with open(classes_path) as f:
-        class_names = f.readlines()
-    class_names = [c.strip() for c in class_names]
-    return class_names
-
-
-def get_anchors(anchors_path):
-    '''loads the anchors from a file'''
-    with open(anchors_path) as f:
-        anchors = f.readline()
-    anchors = [float(x) for x in anchors.split(',')]
-    return np.array(anchors).reshape(-1, 2)
-
-
-def data_generator_wrapper(
-    annotation_lines, batch_size, input_shape, anchors, num_classes
-):
-    n = len(annotation_lines)
-    if n == 0 or batch_size <= 0:
-        return None
-    return data_generator(
-        annotation_lines, batch_size, input_shape, anchors, num_classes)
-
-
-def data_generator(
-    annotation_lines, batch_size, input_shape, anchors, num_classes
-):
-    '''data generator for fit_generator'''
-    n = len(annotation_lines)
-    i = 0
-    while True:
-        image_data = []
-        box_data = []
-        for b in range(batch_size):
-            if i == 0:
-                np.random.shuffle(annotation_lines)
-            image, box = get_random_data(
-                annotation_lines[i], input_shape, random=True)
-            image_data.append(image)
-            box_data.append(box)
-            i = (i+1) % n
-        image_data = np.array(image_data)
-        box_data = np.array(box_data)
-        y_true = preprocess_true_boxes(
-            box_data, input_shape, anchors, num_classes)
-        yield [image_data, *y_true], np.zeros(batch_size)
-
-
-def rand(a=0, b=1):
-    return np.random.rand()*(b-a) + a

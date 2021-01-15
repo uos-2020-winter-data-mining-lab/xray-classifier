5b2b39f42ca44cefd0aa47afc5568e7fcacf2a7b
handal95 <45425838+handal95@users.noreply.github.com>
2021-01-15 19:27:35 +0900

    split darknet and yolo model

--- app/hans/models/train.py
+++ app/hans/models/train.py
@@ -2,39 +2,25 @@
 Retrain the YOLO model for your own dataset.
 """
 
-import numpy as np
 import keras.backend as K
-from keras.layers import Input, Lambda
 from keras.models import Model
-from keras.optimizers import Adam
+from keras.layers import Input, Lambda
 
 from .model import yolo_body, yolo_loss
-from app.hans.config import INPUT_SHAPE, NUM_CLASSES
-
-
-def get_classes(classes_path):
-    '''loads the classes'''
-    with open(classes_path) as f:
-        class_names = f.readlines()
-    class_names = [c.strip() for c in class_names]
-    return class_names
-
-
-def get_anchors(anchors_path):
-    '''loads the anchors from a file'''
-    with open(anchors_path) as f:
-        anchors = f.readline()
-    anchors = [float(x) for x in anchors.split(',')]
-    return np.array(anchors).reshape(-1, 2)
 
 
 def create_model(
-    anchors, load_pretrained=True, freeze_body=2, weights='data/yolov3.h5'
+    input_shape,
+    anchors,
+    num_classes,
+    freeze_body=2,
+    weights=None,
+    summary=False
 ):
     '''create the training model'''
     K.clear_session()  # get a new session
     image_input = Input(shape=(None, None, 3))
-    h, w = INPUT_SHAPE
+    h, w = input_shape
     num_anchors = len(anchors)
 
     y_true = [
@@ -42,24 +28,29 @@ def create_model(
             h//{0: 32, 1: 16, 2: 8}[layer],
             w//{0: 32, 1: 16, 2: 8}[layer],
             num_anchors // 3,
-            NUM_CLASSES + 5)
+            num_classes + 5)
         ) for layer in range(3)
     ]
 
-    model_body = yolo_body(image_input, num_anchors//3, NUM_CLASSES)
-    print(f'Create YOLOv3 model with {num_anchors} anchors '
-          f'and {NUM_CLASSES} classes.')
+    model_body = yolo_body(image_input, num_anchors//3, num_classes)
+    print(f'>> Create YOLOv3 model '
+          f'with {num_anchors} anchors and {num_classes} classes.')
 
-    if load_pretrained is True:
+    if weights:
+        print(f'>> Load weights "{weights}"')
         model_body.load_weights(weights, by_name=True, skip_mismatch=True)
-        print(f'Load weights "{weights}"')
-        if freeze_body in [1, 2]:
-            # Freeze darknet53 body or freeze all but 3 output layers.
-            num = (185, len(model_body.layers)-3)[freeze_body-1]
-            for i in range(num):
+
+        if freeze_body in [0, 1, 2]:
+            # 0: Freeze None
+            # 1: Freeze Darknet53 body
+            # 2: Freeze All but 3 output layers
+            layers = len(model_body.layers)
+            freezing_num = (0, 185, layers-3)[freeze_body]
+            for i in range(freezing_num):
                 model_body.layers[i].trainable = False
-            print(f'Freeze the First {num} layers of '
-                  f'total {len(model_body.layers)}')
+
+            print(f'>> Freeze the First {freezing_num} layers of '
+                  f'total {layers}')
 
     model_loss = Lambda(
         yolo_loss,
@@ -67,11 +58,14 @@ def create_model(
         name='yolo_loss',
         arguments={
             'anchors': anchors,
-            'num_classes': NUM_CLASSES,
+            'num_classes': num_classes,
             'ignore_thresh': 0.5
         }
     )([*model_body.output, *y_true])
 
     model = Model([model_body.input, *y_true], model_loss)
+
+    if summary is True:
+        model.summary(positions=[.35, .65, .73, 1.])
 
     return model
--- app/hans/run.py
+++ app/hans/run.py
@@ -7,16 +7,13 @@ from keras.callbacks import ReduceLROnPlateau, EarlyStopping
 from app.hans.config import INPUT_SHAPE, BATCH_SIZE
 from app.hans.plot import plotting
 from .models.train import create_model
-from .utils.data import read_file
-from .utils.split import data_split, data_generator
 
+from app.hans.process.metadata import read_meta_files
+from app.hans.process.process import split, generator
 
-DATA_DIR = 'data/'
-RAW_DATA = f'{DATA_DIR}/raw/dataset'
 
 config = tf.compat.v1.ConfigProto(
     gpu_options=tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.8)
-    # device_count = {'GPU': 1}
 )
 config.gpu_options.allow_growth = True
 session = tf.compat.v1.Session(config=config)
@@ -24,39 +21,46 @@ tf.compat.v1.keras.backend.set_session(session)
 
 
 def run():
-    # Step 0 . Path Config
-    data = read_file('annotation')
-    class_names, num_classes = read_file('classes')
-    anchors = read_file('anchors')
+    # Step 0 . Config
+    data, num_classes, anchors = read_meta_files(meta_dir='data/metadata')
+    input_shape, batch_size = INPUT_SHAPE, BATCH_SIZE
 
     # Step 1 . Data Preprocessing
-    (train_data, valid_data), (num_train, num_valid) = data_split(data)
+    train_data, valid_data = split(data)
 
     # Step 2. Data Generating
-    train_generator = data_generator(train_data, anchors)
-    valid_generator = data_generator(valid_data, anchors)
+    train_generator = generator(
+        train_data, input_shape, batch_size, anchors, num_classes)
+    valid_generator = generator(
+        valid_data, input_shape, batch_size, anchors, num_classes)
 
     # Step 3. Model Setting
-    model = create_model(anchors, freeze_body=2, weights='data/yolov3-320.h5')
-    # model.summary(positions=[.35, .65, .73, 1.])
+    model = create_model(
+        input_shape=input_shape,
+        anchors=anchors,
+        num_classes=num_classes,
+        freeze_body=2,
+        weights='data/yolov3-320.h5',
+        summary=False
+    )
 
     # Step 4. Model Compile
-    # model.compile(
-    #     optimizer=Adam(lr=1e-4),
-    #     loss={'yolo_loss': lambda y_true, y_pred: y_pred},
-    # )
+    model.compile(
+        optimizer=Adam(lr=1e-4),
+        loss={'yolo_loss': lambda y_true, y_pred: y_pred},
+    )
 
     # Step 5. Model Fitting
-    # reduce_lr = ReduceLROnPlateau(factor=0.1, patience=3, verbose=1)
-    # early_stopping = EarlyStopping(min_delta=0, patience=10, verbose=1)
+    reduce_lr = ReduceLROnPlateau(factor=0.1, patience=3, verbose=1)
+    early_stopping = EarlyStopping(min_delta=0, patience=10, verbose=1)
 
-    # history = model.fit(
-    #     train_generator,
-    #     steps_per_epoch=max(1, num_train//batch_size),
-    #     validation_data=valid_generator,
-    #     validation_steps=max(1, num_valid//batch_size),
-    #     epochs=100,
-    #     callbacks=[reduce_lr, early_stopping])
+    history = model.fit(
+        train_generator,
+        steps_per_epoch=max(1, len(train_data)//batch_size),
+        validation_data=valid_generator,
+        validation_steps=max(1, len(valid_data)//batch_size),
+        epochs=2,
+        callbacks=[reduce_lr, early_stopping])
 
     # Step 6. Plotting
     # plotting(history)
